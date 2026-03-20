84822fddd8b3743a6090d7167a090f4c23f0538f
saint miracle <nanagyeman79@gmail.com>
2026-03-20 23:44:53 +0000

    first commit

---
+++ config.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+
+if (fs.existsSync('config.env')) {
+  dotenv.config({ path: './config.env' });
+} else if (fs.existsSync('.env')) {
+  dotenv.config();
+}
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "",
+// add your Session Id 
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
+// make true or false status auto seen
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+// make true if you want auto reply on status 
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
+// make true if you want auto reply on status 
+ANTI_CALL: process.env.ANTI_CALL || "false",
+// make true if you want anti call system
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY WIZCODER-MD 🤍*",
+// set the auto reply massage on status reply  
+MENTION_REPLY: process.env.MENTION_REPLY || "false",
+// make true if want auto voice reply if someone menetion you 
+PREFIX: process.env.PREFIX || ".",
+// add your prifix for bot   
+STICKER_NAME: process.env.STICKER_NAME || "WIZCORDER-MD\ndo .allvar to change sticker pack name",
+// type sticker pack name 
+CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+// make this true for custum emoji react    
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,🚹,🤎,🚹,🤍,💕,😘,🤧,😊,😁,😍,😃,😋,😎,😅,🥰,😣,🤐,😒,🫠,😈,😻,😂,🤣",
+// chose custom react emojis by yourself 
+OWNER_NUMBER: process.env.OWNER_NUMBER || "233206777968",
+// add your bot owner number
+OWNER_NAME: process.env.OWNER_NAME || "WIZCODER",
+// add bot owner name
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+// Turn true or false for automatic read msgs
+AUTO_REACT: process.env.AUTO_REACT || "false",
+// make this true or false for auto react on all msgs
+ANTI_BAD: process.env.ANTI_BAD || "false",
+// false or true for anti bad words  
+MODE: process.env.MODE || "public",
+// make bot public-private-inbox-group 
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
+// make true for send automatic voices
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+// make true for automatic stickers 
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+// make true or false automatic text reply 
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+// maks true for always online 
+PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
+// make false if want private mod
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+// true for automatic show typing   
+READ_CMD: process.env.READ_CMD || "false",
+// true if want mark commands as read 
+DEV: process.env.DEV || "233206777968",
+//replace with your whatsapp number        
+ANTI_VV: process.env.ANTI_VV || "false",
+// true for anti once view 
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log",
+// Set "log" to send deleted messages to owner DM, "group" to send in the same group
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
+// make it true for auto recording 
+};
